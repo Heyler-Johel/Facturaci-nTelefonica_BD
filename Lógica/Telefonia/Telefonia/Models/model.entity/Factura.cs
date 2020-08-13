f472e78e567a1865d6037b467c634559a069c07c
Heyler-Johel <johelmora@gmail.com>
2020-08-13 15:49:11 -0600

    Capa lógica, correcta.

---
+++ Lógica/Telefonia/Telefonia/Models/model.entity/Factura.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace model.entity
+{
+    public class Factura
+    {
+        public int ID { get; set; }
+        public string Fecha { get; set; }
+        public string FechaAPagar { get; set; }
+        public decimal SaldoMega { get; set; }
+        public decimal SaldoMinutos { get; set; }
+        public decimal SaldoMins110 { get; set; }
+        public decimal SaldoMins800 { get; set; }
+        public decimal SaldoMins900 { get; set; }
+        public decimal SaldoTotalPa { get; set; }
+        public bool estado { get; set; }
+
+        public Factura() { }
+
+        public Factura (int pid)
+        {
+            ID = pid;
+        }
+
+    }
+}
