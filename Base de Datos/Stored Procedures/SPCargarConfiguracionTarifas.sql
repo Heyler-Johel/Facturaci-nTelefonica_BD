040c01abd11200296cf509f7c2a19f724dab4b13
KevinFallas03 <48746116+KevinFallas03@users.noreply.github.com>
2020-08-14 01:02:55 -0600

    Cambio de nombre del sp

--- Base de Datos/Stored Procedures/SPCargarConfiguracionTarifas.sql
+++ Base de Datos/Stored Procedures/SPCargarConfiguracionTarifas.sql
@@ -6,48 +6,48 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE OR ALTER PROCEDURE [dbo].[spCargarDatosCC]
+CREATE OR ALTER PROCEDURE [dbo].[spCargarConfiguracionTarifas]
 AS
 BEGIN
-    SET NOCOUNT ON;                                                                                                                                                                                                                                                                                             
+	SET NOCOUNT ON;                                                                                                                                                                                                                                                                                             
 
-    -- VARIABLES --
-    DECLARE @Configuracion XML
+	-- VARIABLES --
+	DECLARE @Configuracion XML
 
-    BEGIN TRY
+	BEGIN TRY
 		
-        --Insercion de los tipos de CCobro
-        SELECT @Configuracion = Cf
-        FROM OPENROWSET (Bulk 'D:\Base de datos\FacturacionTelefonica_BD\Base de Datos\XML\configuracionTarifas.xml', Single_BLOB) 
+		--Insercion de los tipos de CCobro
+		SELECT @Configuracion = Cf
+		FROM OPENROWSET (Bulk 'D:\Base de datos\FacturacionTelefonica_BD\Base de Datos\XML\configuracionTarifas.xml', Single_BLOB) 
 		AS Configuracion(Cf);
 
 		--Insertamos el tipo de relacion familiar
-        INSERT INTO [dbo].[TipoRelacion](ID, Nombre)
+		INSERT INTO [dbo].[TipoRelacion](ID, Nombre)
 		SELECT	cf.value('@ID', 'INT')
-			  ,	cf.value('@Nombre', 'VARCHAR(100)')
+				,	cf.value('@Nombre', 'VARCHAR(100)')
 		FROM @Configuracion.nodes('/configTarifas/TipoRelacionFamiliar') AS t(cf);
 		
-		INSERT INTO [dbo].[TipoTarifa](ID, Identificacion)
+		INSERT INTO [dbo].[TipoTarifa](ID, Nombre)
 		SELECT	tt.value('@ID','INT')
-			 ,	tt.value('@Nombre', 'VARCHAR(100)')
+				,	tt.value('@Nombre', 'VARCHAR(100)')
 		FROM @Configuracion.nodes('/configTarifas/TipoTarifa') AS t(tt);
 
 		INSERT INTO [dbo].[TipoElemento](ID, Nombre, TipoValor)
 		SELECT	te.value('@ID', 'INT')
-			  ,	te.value('@Nombre', 'VARCHAR(100)')
-			  , te.value('@TipoValor', 'VARCHAR(100)')
+				,	te.value('@Nombre', 'VARCHAR(100)')
+				, te.value('@TipoValor', 'VARCHAR(100)')
 		FROM @Configuracion.nodes('/configTarifas/TipoElemento') AS t(te);
 
 		INSERT INTO [dbo].[ElementoDeTipoTarifa](IdTipoTarifa, IdTipoElemento, Valor)
 		SELECT et.value('@IDTipoTarifa', 'INT')
-			 , et.value('@IDTipoElemento', 'INT')
-			 , et.value('@Valor', 'MONEY')
+				, et.value('@IDTipoElemento', 'INT')
+				, et.value('@Valor', 'MONEY')
 		FROM @Configuracion.nodes('/configTarifas/ElementoDeTipoTarifa') AS t(et);
 
 		RETURN 1
 		
-    END TRY
-    BEGIN CATCH
-        RETURN @@ERROR * -1
-    END CATCH
- END
+	END TRY
+	BEGIN CATCH
+		RETURN @@ERROR * -1
+	END CATCH
+END
